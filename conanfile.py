aad484397ae423662492a239b530a34ed87f5036
Diego Rodrigues <diejorarr@gmail.com>
2025-05-03 15:26:13 -0500

    maybe final revision conan-slang

--- conanfile.py
+++ conanfile.py
@@ -1,56 +1,117 @@
-import os
+import os, glob
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
-from conan.tools.files import copy
 from conan.tools.scm import Git
+from conan.tools.files import (
+    rmdir, collect_libs, copy, update_conandata,
+)
 
 class SlangConan(ConanFile):
-    name = "slang"
-    version = "2025.6.3"
-    author = "shader-slang"  
-    description = "Slang is a shader tool and compiler for modern GPU shading languages."
-    topics = ("conan", "slang", "shader", "webgpu")
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": True, "fPIC": True}
-    export_sources = "LICENSE*", "LICENSES/*"
+    name         = "slang"
+    version      = "2025.6.3"
+    license      = "MIT"      # if you know it
+    url          = "https://github.com/shader-slang/slang"
+    homepage     = "https://github.com/shader-slang/slang"
+    description  = "Slang is a shader tool and compiler for modern GPU shading languages."
+    topics       = ("conan", "slang", "shader", "webgpu")
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            del self.options.fPIC
+    settings     = "os", "compiler", "build_type", "arch"
+    options      = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # ----------------------------------------------------------------------------
+    def export(self):
+        git = Git(self, self.recipe_folder)
+        url, commit = git.get_url_and_commit()
+        update_conandata(self, {"sources": {"url": url, "commit": commit}})
 
     def layout(self):
         cmake_layout(self)
 
     def source(self):
-        # Checkout the Slang repository at the specific tag, initializing submodules with --recursive.
+        src = self.conan_data["sources"]
         git = Git(self)
-        git.clone(url="https://github.com/shader-slang/slang.git",
-                  branch="v2025.6.3",
-                  args="--recursive",
-                  target=".")
+
+        git.clone(
+            url=src["url"],
+            target=".",
+            args=[
+                "--branch",     src["commit"],
+                "--single-branch",
+                "--depth",      "1",
+            ]
+        )
+
+        git.update_submodules(init=True, recursive=True, args=["--depth", "1"])
+
+        rmdir(self, os.path.join(self.source_folder, ".git"))
 
     def generate(self):
         tc = CMakeToolchain(self, generator="Ninja")
-        # Enable generation of release builds with debug info (similar to releaseWithDebugInfo preset).
-        tc.variables["SLANG_ENABLE_RELEASE_DEBUG_INFO"] = "ON"
+        tc.variables["BUILD_SHARED_LIBS"]       = "ON" if self.options.shared else "OFF"
+        tc.variables["SLANG_LIB_TYPE"]         = "SHARED" if self.options.shared else "STATIC"
+        if self.options.get_safe("fPIC"):
+            tc.variables["CMAKE_POSITION_INDEPENDENT_CODE"] = "ON"
+
+        # keep debug info = OFF and LTO = OFF by default for *.a size reasons
+        tc.variables["SLANG_ENABLE_RELEASE_DEBUG_INFO"] = "OFF"
+        tc.variables["SLANG_ENABLE_RELEASE_LTO"]        = "OFF"
+
+        # other feature flags
+        tc.variables["SLANG_SLANG_LLVM_FLAVOR"]    = "DISABLE"
+        tc.variables["SLANG_ENABLE_DXIL"]          = "OFF"
+        tc.variables["SLANG_ENABLE_SLANGD"]        = "OFF"
+        tc.variables["SLANG_ENABLE_GFX"]           = "OFF"
+        tc.variables["SLANG_ENABLE_SLANG_GLSLANG"] = "OFF"
+        tc.variables["SLANG_ENABLE_TESTS"]         = "OFF"
+        tc.variables["SLANG_ENABLE_EXAMPLES"]      = "OFF"
         tc.generate()
 
     def build(self):
         cmake = CMake(self)
-        if self.settings.os == "Windows":
-            # Optionally add Windows-specific flags.
-            cmake.configure(variables={"CMAKE_CXX_FLAGS": "/utf-8"})
-        else:
-            cmake.configure()
+        cmake.configure()
         cmake.build()
+
+    def _copy_archives(self, patterns):
+        cand_dirs = [
+            os.path.join(self.build_folder, "lib"),
+            os.path.join(self.build_folder, str(self.settings.build_type), "lib"),
+        ]
+        for d in cand_dirs:
+            for pat in patterns:
+                for f in glob.glob(os.path.join(d, pat)):
+                    copy(self, os.path.basename(f),
+                         src=d, dst=os.path.join(self.package_folder, "lib"))
 
     def package(self):
         cmake = CMake(self)
         cmake.install()
+        if not self.options.shared:
+            # pick up the extra .a files we need
+            self._copy_archives(["libcore*.a",
+                                 "libcompiler-core*.a",
+                                 "libslang-cpp-parser*.a"])
+            # third-party
+            copy(self, "libminiz*.a",
+                 src=os.path.join(self.build_folder, "external", "miniz"),
+                 dst=os.path.join(self.package_folder, "lib"))
+            copy(self, "liblz4*.a",
+                 src=os.path.join(self.build_folder,
+                                  "external", "lz4", "build", "cmake"),
+                 dst=os.path.join(self.package_folder, "lib"))
 
     def package_info(self):
-        # Define the exported CMake target.
+        if self.options.shared:
+            self.cpp_info.libs = ["slang"]
+        else:
+            # static link order
+            self.cpp_info.libs = [
+                "slang", "compiler-core", "core", "slang-cpp-parser",
+                "slang-rt", "miniz", "lz4",
+            ]
         self.cpp_info.set_property("cmake_target_name", "slang::slang")
-        self.cpp_info.libs = ["slang"]
+        if self.settings.os == "Linux":
+            self.cpp_info.system_libs = ["dl", "pthread", "m"]
+        elif self.settings.os == "Windows":
+            self.cpp_info.system_libs = ["Advapi32", "Kernel32", "User32"]
 
